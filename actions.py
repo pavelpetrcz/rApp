5c24f8796db022caca5f98fb2ec7ae94f2ebf900
pavelpetrcz <ppetr@ppetr.cz>
2021-12-31 00:06:30 +0100

    refactor, renamed, fixed problems claimed by IDE, upgraded required version of packages

--- actions.py
+++ actions.py
@@ -2,6 +2,11 @@ from pymongo import MongoClient
 
 
 def getDbConn(connection_string):
+    """
+    create connection to MongoDB
+    :param connection_string:
+    :return: client - object at which you can choose collection from "estates" database
+    """
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(connection_string)
     # Create the database for our example (we will use the same database throughout the tutorial
--- app.py
+++ app.py
@@ -9,12 +9,13 @@ import actions
 """
 Created on Sat Oct 17 19:32:08 2020
 
-@author: pavel
+@author: pavelpetrcz
 """
 
 if __name__ == "__main__":
+    # constants
     again = True
-    dbConnNotEstablished = True
+    collection_name = ""
 
     # logging setup
     logging.basicConfig(
@@ -23,8 +24,18 @@ if __name__ == "__main__":
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
+    # establish db connection
+    try:
+        conn_string = 'mongodb+srv://ppetr:{}@clusterppe.hga2z.mongodb.net/estates?retryWrites=true&w=majority'.format(os.environ['db_pass'])
+        clientDb = actions.getDbConn(connection_string=conn_string)
+        collection_name = clientDb["flats"]
+    except Exception as e:
+        logging.error(e, stack_info=True, exc_info=True)
+
+    # scrape content
     while again:
         try:
+            # get list of estates
             estates_list = re.get(
                 'http://sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&locality_region_id=1&per_page=10&tms=1640380327573')
             estates_list = json.loads(estates_list.text)
@@ -34,19 +45,14 @@ if __name__ == "__main__":
             for estate in estates:
                 estateIds.append(estate['hash_id'])
 
-            if len(estateIds) > 0 and dbConnNotEstablished:
-                conn_string = 'mongodb+srv://ppetr:{}@clusterppe.hga2z.mongodb.net/estates?retryWrites=true&w=majority'.format(
-                    os.environ['db_pass'])
-                clientDb = actions.getDbConn(connection_string=conn_string)
-                collection_name = clientDb["flats"]
-
+            # get detail of estate and save
             for estateId in estateIds:
                 url = "https://www.sreality.cz/api/cs/v2/estates/{}?tms=1640382199242".format(estateId)
                 estate_detail_offer = re.get(url)
                 dict_estate_detail_offer = json.loads(estate_detail_offer.text)
 
-                objects_to_delete = ['map',
-                                     'meta_description',
+                # delete unwated objects from json
+                objects_to_delete = ['meta_description',
                                      '_embedded',
                                      'logged_in',
                                      'is_topped',
@@ -61,11 +67,14 @@ if __name__ == "__main__":
 
                 for obj in objects_to_delete:
                     del dict_estate_detail_offer[obj]
+                del dict_estate_detail_offer['map']['geometry']
 
+                # save to DB
                 collection_name.insert_one(dict_estate_detail_offer)
 
+                # wait a while to scrape next
                 time.sleep(10)
 
-        except Exception:
-            logging.warning('Offer was not saved.', stack_info=True)
+        except Exception as e:
+            logging.warning(e, stack_info=True, exc_info=True)
             continue
