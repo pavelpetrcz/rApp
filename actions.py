a1ffe5de95f367bf890359884bb8e103d539bfa8
pavelpetrcz <ppetr@ppetr.cz>
2022-01-02 22:54:33 +0100

    added scheduling - scraping is done every 24 hours

--- actions.py
+++ actions.py
@@ -1,13 +1,70 @@
+#  Copyright (c) Pavel Petr 2021.
+import logging
+import time
+
 from pymongo import MongoClient
+
+import requests as re
+import json
 
 
 def getDbConn(connection_string):
     """
     create connection to MongoDB
-    :param connection_string:
+    :param connection_string: insert specified URI to MongoDB database location
     :return: client - object at which you can choose collection from "estates" database
     """
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(connection_string)
     # Create the database for our example (we will use the same database throughout the tutorial
     return client['estates']
+
+
+def scrapeContent(collection_name):
+    """
+    Method extract list of 100 lastly added estates an save each of them to DB
+    :param collection_name: insert object from mongo client with specified collection where to store data
+    """
+    try:
+        # get list of estates
+        estates_list = re.get(
+            'http://sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&locality_region_id=1&per_page=10&tms=1640380327573')
+        estates_list = json.loads(estates_list.text)
+        estates = estates_list['_embedded']['estates']
+
+        estateIds = []
+        for estate in estates:
+            estateIds.append(estate['hash_id'])
+
+        # get detail of estate and save
+        for estateId in estateIds:
+            url = "https://www.sreality.cz/api/cs/v2/estates/{}?tms=1640382199242".format(estateId)
+            estate_detail_offer = re.get(url)
+            dict_estate_detail_offer = json.loads(estate_detail_offer.text)
+
+            # delete unwated objects from json
+            objects_to_delete = ['meta_description',
+                                 '_embedded',
+                                 'logged_in',
+                                 'is_topped',
+                                 '_links',
+                                 'panorama',
+                                 'seo',
+                                 'rus',
+                                 'is_topped_today',
+                                 'locality_district_id',
+                                 'codeItems'
+                                 ]
+
+            for obj in objects_to_delete:
+                del dict_estate_detail_offer[obj]
+            del dict_estate_detail_offer['map']['geometry']
+
+            # save to DB
+            collection_name.insert_one(dict_estate_detail_offer)
+
+            # wait a while to scrape next
+            time.sleep(10)
+
+    except Exception as exce:
+        logging.warning(exce, stack_info=True, exc_info=True)
--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-import requests as re
-import json
+#  Copyright (c) Pavel Petr 2021.
+
+
 import time
 import logging
 import os
+import schedule
 
 import actions
-
-"""
-Created on Sat Oct 17 19:32:08 2020
-
-@author: pavelpetrcz
-"""
 
 if __name__ == "__main__":
     # constants
@@ -26,55 +22,20 @@ if __name__ == "__main__":
 
     # establish db connection
     try:
-        conn_string = 'mongodb+srv://ppetr:{}@clusterppe.hga2z.mongodb.net/estates?retryWrites=true&w=majority'.format(os.environ['db_pass'])
+        conn_string = 'mongodb+srv://ppetr:{}@clusterppe.hga2z.mongodb.net/estates?retryWrites=true&w=majority'.format(
+            os.environ['db_pass'])
         clientDb = actions.getDbConn(connection_string=conn_string)
         collection_name = clientDb["flats"]
     except Exception as e:
         logging.error(e, stack_info=True, exc_info=True)
 
-    # scrape content
+    # scrape content every 24 hours
+    try:
+        schedule.every(24).hours.do(actions.scrapeContent, collection_name=collection_name)
+    except Exception as exc:
+        logging.warning(exc, stack_info=True, exc_info=True)
+
+    # run schedule
     while again:
-        try:
-            # get list of estates
-            estates_list = re.get(
-                'http://sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&locality_region_id=1&per_page=10&tms=1640380327573')
-            estates_list = json.loads(estates_list.text)
-            estates = estates_list['_embedded']['estates']
-
-            estateIds = []
-            for estate in estates:
-                estateIds.append(estate['hash_id'])
-
-            # get detail of estate and save
-            for estateId in estateIds:
-                url = "https://www.sreality.cz/api/cs/v2/estates/{}?tms=1640382199242".format(estateId)
-                estate_detail_offer = re.get(url)
-                dict_estate_detail_offer = json.loads(estate_detail_offer.text)
-
-                # delete unwated objects from json
-                objects_to_delete = ['meta_description',
-                                     '_embedded',
-                                     'logged_in',
-                                     'is_topped',
-                                     '_links',
-                                     'panorama',
-                                     'seo',
-                                     'rus',
-                                     'is_topped_today',
-                                     'locality_district_id',
-                                     'codeItems'
-                                     ]
-
-                for obj in objects_to_delete:
-                    del dict_estate_detail_offer[obj]
-                del dict_estate_detail_offer['map']['geometry']
-
-                # save to DB
-                collection_name.insert_one(dict_estate_detail_offer)
-
-                # wait a while to scrape next
-                time.sleep(10)
-
-        except Exception as e:
-            logging.warning(e, stack_info=True, exc_info=True)
-            continue
+        schedule.run_pending()
+        time.sleep(1)
